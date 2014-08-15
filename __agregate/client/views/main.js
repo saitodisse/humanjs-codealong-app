f9b8bc11223a28c3d48d0055cf507ff114d426ae
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-15 16:39:15 -0300

    11 - Selecting active nav/

--- __agregate/client/views/main.js
+++ __agregate/client/views/main.js
@@ -1,5 +1,6 @@
 var View = require('ampersand-view');
 var ViewSwitcher = require('ampersand-view-switcher');
+var dom = require('ampersand-dom');
 var templates = require('../templates');
 
 
@@ -18,13 +19,24 @@ module.exports = View.extend({
     },
     handleNewPage: function (page) {
         this.pages.set(page);
+        this.updateActiveNav();
     },
     handleLinkClick: function (e) {
         var aTag = e.target;
         if (aTag.host === window.location.host) {
-            // this is a local click
             app.router.history.navigate(aTag.pathname, {trigger: true});
             e.preventDefault();
         }
+    },
+    updateActiveNav: function () {
+        var pathname = window.location.pathname;
+        this.getAll('nav a').forEach(function (aTag) {
+            var parent = aTag.parentNode;
+            if (pathname.indexOf(aTag.pathname) === 0) {
+                dom.addClass(parent, 'active');
+            } else {
+                dom.removeClass(parent, 'active');
+            }
+        });
     }
 });
