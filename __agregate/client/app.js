8d8957d08db01e3cb2f65f9b56865e55b4d637a2
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-15 16:39:48 -0300

    16 - Creating a collection for our restful API/

--- __agregate/client/app.js
+++ __agregate/client/app.js
@@ -2,12 +2,17 @@ var domready = require('domready');
 var MainView = require('./views/main');
 var Router = require('./router');
 var MeModel = require('./models/me');
+var Persons = require('./models/persons');
 
 
 window.app = {
     init: function () {
         var self = this;
         window.me = new MeModel();
+
+        // data collections
+        this.persons = new Persons();
+
         domready(function () {
             self.router = new Router();
             self.view = new MainView({
