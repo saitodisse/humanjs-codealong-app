e4c41f8ea241db99094ebc86b5548e33a812714b
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-15 16:38:26 -0300

    04 - creating the router/

--- __agregate/client/app.js
+++ __agregate/client/app.js
@@ -1,14 +1,17 @@
-var MainView = require('./main-view');
 var domready = require('domready');
+var MainView = require('./main-view');
+var Router = require('./router');
 
 
 window.app = {
     init: function () {
         var self = this;
         domready(function () {
+            self.router = new Router();
             self.view = new MainView({
                 el: document.body
             });
+            self.router.history.start({pushState: true});
         });
     }
 };
