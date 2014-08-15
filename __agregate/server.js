d411f8cff4de0cbe1802f4a974af2039ae230b0d
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-15 16:39:03 -0300

    08 - stylizer and stylus/

--- __agregate/server.js
+++ __agregate/server.js
@@ -3,6 +3,7 @@ var server = hapi.createServer(8080, 'localhost');
 var moonboots = require('moonboots_hapi');
 var config = require('getconfig');
 var templatizer = require('templatizer');
+var stylizer = require('stylizer');
 
 
 server.pack.register({
@@ -13,12 +14,25 @@ server.pack.register({
             main: __dirname + '/client/app.js',
             developmentMode: config.isDev,
             stylesheets: [
-                __dirname + '/public/bootstrap.css'
+                __dirname + '/public/bootstrap.css',
+                __dirname + '/public/app.css',
             ],
             beforeBuildJS: function () {
                 if (config.isDev) {
                     templatizer(__dirname + '/templates', __dirname + '/client/templates.js');
                 }
+            },
+            beforeBuildCSS: function (done) {
+                if (!config.isDev) return done();
+
+
+
+                stylizer({
+                    infile: __dirname + '/public/app/main.styl',
+                    outfile: __dirname + '/public/app.css',
+                    development: true,
+                    watch: __dirname + '/public/app/**/*.styl'
+                }, done);
             }
         }
     }
